1f34d5bbbde961dbfcb8152745bf8ced4263bf5a
Erin Matheny <erin_m713@yahoo.com>
2020-10-27 20:46:16 -0500

    working on retrieving history for specific order. need .finally or new backend route.

--- frontend/src/components/Detailedhistory.js
+++ frontend/src/components/Detailedhistory.js
@@ -1,31 +1,69 @@
 import React from 'react';
 import axios from 'axios';
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 class Detailedhistory extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            isLoaded: false
+        };
     }
 
     componentDidMount() {
 
+        var that = this
+
         axios.get(`http://localhost:8000/order-history/${this.props.order_number}`)
             .then(res => {
-                const orderHistory = res.data
-                this.setState(orderHistory)
-                console.log(this.state)
+                let orderDetails = res.data
+
+                orderDetails.map((item, index) => (
+
+                    axios.get(`http://localhost:8000/inventory/${item.inventory_id}`)
+                        .then(details => {
+                            item.key = item.inventory_id
+
+                            item.product_name = details.data[0].product_name
+
+                            that.setState({isLoaded: true})
+                        })
+                        .catch(error => {
+                            console.log('nested axios error')
+                        })
+                ))
+                that.setState({ order: orderDetails })
+
+
             })
             .catch(error => {
                 console.log('error')
             })
+
     }
 
 
     render() {
         return (
             <div>
-                Order History for Specific Order Number
+                {this.state.order && this.state.isLoaded && (
+                    <Container>
+                        Items ordered for order #{this.props.order_number}
+                        <Row>
+                            {this.state.order.map((item, index) => {
+                                return (
+                                    <div key={item.id}>
+                                        
+                                        {console.log(item.product_name)}
+                                    hi
+                                    </div>
+                                )
+                            })}
+                        </Row>
+                    </Container>
+                )}
             </div>
         )
 
