479429cdaf8748c00e0303993d319cf58de9b73f
zac-evans <zacevanscoding@gmail.com>
2020-10-29 00:15:04 -0400

    removed unused imports

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,15 +1,11 @@
 import React from "react";
-import AddToCartButton from "./AddToCartButton";
 import OrderCard from "./OrderCard";
 import { Fade } from "react-awesome-reveal";
 import ProductRating from "./ProductRating";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
-
-import Rating from "@material-ui/lab/Rating";
 
 class Product extends React.Component {
   render() {
